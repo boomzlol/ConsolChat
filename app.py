d18d2857701d94cc1d870f5eb80f71f894bc773a
Pretzz <88212436+btos34@users.noreply.github.com>
2022-05-15 13:34:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import rich
+import os
+import time
+import random
+
+# Rich Setup
+from rich.console import Console
+console = Console()
+
+
+# The Starting Page
+console.print("[green]Terminal[/green] [red]Chatting[/red]", style= "bold")
+console.print("     [[cyan]Enter[/cyan]]")
+input()
+os.system("cls")
+# Input User
 
