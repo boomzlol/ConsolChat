c42cadf68fe79c8f6caf3f581fd66bbbbb35eade
Pretzz <88212436+btos34@users.noreply.github.com>
2022-05-15 14:45:29 -0400

    username checker
    it makes the username under 10 characters and removes all capitals

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import os
 import time
 import random
 
+# List of letters allowed
+Allowed = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
 # Rich Setup
 from rich.console import Console
 console = Console()
@@ -14,4 +17,14 @@ console.print("     [[cyan]Enter[/cyan]]")
 input()
 os.system("cls")
 # Input User
+
+console.print("[magenta] Username? [/magenta]")
+print("(Uppercase letters will be turned lowercase.)")
+print("(Anything longer than 9 characters will be deleted.)")
+print("(Special characters are allowed.)")
+hostname = input(">> ")[:9]
+hostname.lower()
+
+
+input()
 
